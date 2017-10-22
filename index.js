cb4dbe56bb5eb4695fb21ea055266cef18203d1b
Ajay Srinivas <ajay.s1997@gmail.com>
2017-10-22 08:07:47 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+var app = express();
+
+app.engine('hbs', hbs({extname: 'hbs', layoutsDir: __dirname + '/views'}));
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function (req, res) {
+  res.render('index', {
+    title: 'Tourism Management System'
+  });
+});
+
+app.listen(3000, function (err) {
+  if (err) throw err;
+  console.log("Magic happens on port 3000...");
+});
